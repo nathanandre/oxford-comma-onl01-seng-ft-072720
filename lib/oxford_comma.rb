e961578d56dfe70850cb17014b066db9cb80cb95
Nathan Burns <nathanandreburns@gmail.com>
2020-08-05 14:15:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
     puts array[0]
-  else array.length == 2 
-    puts array.join
   elsif array.length > 2
     array.insert([-1], "and")
 	array.join(",")
+  else array.length == 2 
+    puts array.join
   end
     
 end
