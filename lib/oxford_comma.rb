e35865dedea6cc5532a2729dda36b9d7750539af
Nathan Burns <nathanandreburns@gmail.com>
2020-08-05 15:40:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
     return array[0]
+  elsif array.length == 2
+    array.join(", ")
   elsif array.length > 2
     array.insert(-1, "and")
 	array.split(",")
