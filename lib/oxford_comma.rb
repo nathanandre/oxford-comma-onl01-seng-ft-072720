615c23e2ce69cb324f366462492d7e69a2f35e43
Nathan Burns <nathanandreburns@gmail.com>
2020-08-05 16:01:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+  if array.length == 1
+    return array[0]
+    
+  elsif array.length == 2
+    array.join(" and ")
+    
+  elsif array.length > 2
+    array[-1].insert(0, "and ")
+	  array.join(", ")
 
+  end
+    
 end
